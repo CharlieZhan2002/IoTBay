afc6f5ebf9fa431ec61782386d234f7c73947dd5
Yunwei-Zhang <62420557+Yunwei-Zhang@users.noreply.github.com>
2022-03-30 14:39:59 +1100

    update user modal

--- lotBay/src/java/uts/isd/model/User.java
+++ lotBay/src/java/uts/isd/model/User.java
@@ -9,5 +9,49 @@ package uts.isd.model;
  * @author zhang
  */
 public class User {
+    private String Username;
+    private String Useremail;
+    private String Userpassword;
+    private String Userdob;
+
+    public User(String Username, String Useremail, String Userpassword, String Userdob) {
+        this.Username = Username;
+        this.Useremail = Useremail;
+        this.Userpassword = Userpassword;
+        this.Userdob = Userdob;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getUseremail() {
+        return Useremail;
+    }
+
+    public void setUseremail(String Useremail) {
+        this.Useremail = Useremail;
+    }
+
+    public String getUserpassword() {
+        return Userpassword;
+    }
+
+    public void setUserpassword(String Userpassword) {
+        this.Userpassword = Userpassword;
+    }
+
+    public String getUserdob() {
+        return Userdob;
+    }
+
+    public void setUserdob(String Userdob) {
+        this.Userdob = Userdob;
+    }
+    
     
 }
