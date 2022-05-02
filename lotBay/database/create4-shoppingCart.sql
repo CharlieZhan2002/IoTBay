70ec61e4f61b0eac66d9469526c031279f5c5b75
sw-Hua <59052152+sw-Hua@users.noreply.github.com>
2022-05-03 00:58:02 +0800

    Delete lotBay directory
    sw-Hua-patch-1 branch 修改, main里没修改

--- lotBay/database/create4-shoppingCart.sql
+++
@@ -1,8 +0,0 @@
-USE IoTBay;
-CREATE TABLE ShoppingCart{
-    customerID INT(10),
-    accountID INT(10),
-    productID INT(10),
-    overdueGoodsID INT(10),
-    productName VarChar(20)
-};
--- lotBay/database/create6-orderLineItem.sql
+++
@@ -1,9 +0,0 @@
-USE IoTBay;
-CREATE TABLE ShoppingCart{
-    orderlineID INT(10),
-    productID INT(10),
-    itemQuantity INT(5),
-    orderUserName VARCHAR(10),
-    productPrice FLOAT(5),
-    ItermPrice FLOAT(5)
-};
